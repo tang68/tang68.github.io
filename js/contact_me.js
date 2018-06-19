428572170e3472ce05dd44bd25337313ea2ee1f0
Ryan Phat Tang <ryanphattang@Ryans-Mac-mini.local>
2018-06-18 23:07:45 -0700

    contact form

--- js/contact_me.js
+++ js/contact_me.js
@@ -17,6 +17,8 @@ $(function() {
       if (firstName.indexOf(' ') >= 0) {
         firstName = name.split(' ').slice(0, -1).join(' ');
       }
+
+      alert("start" + name)
       $this = $("#sendMessageButton");
       $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
       $.ajax({
